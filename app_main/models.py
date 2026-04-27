8ec5beeccef4fbe71d11fc46874cf8af4916b2aa
Abdujaloliddin <jaloliddinjabborqulov9@gmail.com>
2026-04-28 01:17:52 +0500

    feat: initialize Django project with custom user model, product schema, and base configuration

--- app_main/models.py
+++ app_main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+class CustomUser(AbstractUser):
+    phone_number = PhoneNumberField(verbose_name="Telefon raqam", null=True, blank=True)
+    profile_image = models.ImageField(verbose_name="Profile Image", upload_to="profiles/", null=True, blank=True)
+    address = models.CharField(verbose_name="Address", max_length=255)
 
 
 class Product(models.Model):
-    owner = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     name = models.CharField(verbose_name="Maxsulot nomi", max_length=50, unique=True)
     price = models.DecimalField(verbose_name="Maxsulot narxi", max_digits=12, decimal_places=2)
     image = models.ImageField(verbose_name="Maxsulot rasmi", upload_to="media/", null=True, blank=True)
